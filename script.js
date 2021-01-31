705daee0a8f765972648a6da943543ad55e4a5eb
Thomas Nelson <thnlsn@gmail.com>
2021-01-31 14:05:29 -0800

    changed padding on player containers to take into account dice size so it will be more responsive

--- script.js
+++ script.js
@@ -4,9 +4,8 @@
 const score1El = document.querySelector('#score--0'); // Dom element for score 1
 const score2El = document.getElementById('score--1'); // Dom element for score 2
 
-score1El.textContent = 1;
-score2El.textContent = 1;
+score1El.textContent = 0; // Initialize scores as 0
+score2El.textContent = 0; // Initialize scores as 0
 
 const svg = document.querySelector('.svg');
-
 document.querySelector('.svg').innerHTML = dice[5];
