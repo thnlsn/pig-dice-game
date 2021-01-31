e936d6daf41da4c62160e054e8befc4ce3c31e3b
Thomas Nelson <thnlsn@gmail.com>
2021-01-31 15:30:19 -0800

    created score array for total score state

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
 'use strict';
 
-// Selectors for the two score elements
-const score1El = document.querySelector('#score--0'); // Dom element for score 1
-const score2El = document.getElementById('score--1'); // Dom element for score 2
+// Selectors for DOM elements
+const score0El = document.getElementById('score--0'); // DOM element for score 0
+const score1El = document.getElementById('score--1'); // DOM element for score 1
+const dieEl = document.querySelector('.svg'); // DOM element for die SVG (not path)
+const current0El = document.getElementById('current--0'); // DOM element for current 0
+const current1El = document.getElementById('current--1'); // DOM element for current 1
 
+// Starting conditions
+score0El.textContent = 0; // Initialize scores as 0
 score1El.textContent = 0; // Initialize scores as 0
-score2El.textContent = 0; // Initialize scores as 0
 
-const svg = document.querySelector('.svg');
-document.querySelector('.svg').innerHTML = dice[5];
+// Persisting state
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Functions for buttons
+const roll = (active) => {
+  // Generate random number
+  const result = Math.trunc(Math.random() * 6); // Random # 0-5
+  dieEl.innerHTML = dice[result]; // Use that number to search array and set the SVG
+
+  // Check if die is 0 (1 on the SVG)
+  if (result === 0) {
+    // Set currentScore to 0
+    currentScore = 0;
+    // Switch active player
+  } else {
+    // Add roll to current
+    currentScore += result + 1;
+    current0El.textContent = currentScore;
+  }
+};
+
+const reset = () => {
+  dieEl.innerHTML = null;
+};
+
+// Event listeners for buttons
+const newGameBtn = document
+  .querySelector('.controls__new-game')
+  .addEventListener('click', reset);
+const rollBtn = document
+  .querySelector('.controls__roll-dice')
+  .addEventListener('click', roll);
+/* const holdBtn = document
+  .querySelector('.controls__hold')
+  .addEventListener('click', hold); */
