654f646d244f86ca2225dc3cdf2e80dc07da96ef
Thomas Nelson <thnlsn@gmail.com>
2021-01-30 21:53:18 -0800

    die svg images importing and rendering correctly

---
+++ dice.js
@@ -0,0 +1,14 @@
+const dice = [
+  // Die 1
+  `<g id="dice-1"><rect class="die" width="150" height="150" rx="20" /><circle class="die__dot" cx="75.5" cy="75.5" r="12.5" /></g>`,
+  // Die 2
+  `<g id="dice-2"><rect class="die" width="150" height="150" rx="20"/><circle class="die__dot" cx="50.5" cy="50.5" r="12.5"/><circle class="die__dot" cx="100.5" cy="100.5" r="12.5"/></g>`,
+  // Die 3
+  `<g id="dice-3"><rect class="die" width="150" height="150" rx="20" /><circle class="die__dot" cx="50.5" cy="50.5" r="12.5" /><circle class="die__dot" cx="100.5" cy="100.5" r="12.5" /><circle class="die__dot" cx="75.5" cy="75.5" r="12.5" /></g>`,
+  // Die 4
+  `<g id="dice-4"><rect class="die" width="150" height="150" rx="20" /><circle class="die__dot" cx="49.5" cy="50.5" r="12.5" /><circle class="die__dot" cx="99.5" cy="100.5" r="12.5" /><circle class="die__dot" cx="49.5" cy="100.5" r="12.5" /><circle class="die__dot" cx="100.5" cy="50.5" r="12.5" /></g>`,
+  // Die 5
+  `<g id="dice-5"><rect class="die" width="150" height="150" rx="20"/><circle class="die__dot" cx="49.5" cy="50.5" r="12.5"/><circle class="die__dot" cx="99.5" cy="100.5" r="12.5"/><circle class="die__dot" cx="49.5" cy="100.5" r="12.5"/><circle class="die__dot" cx="100.5" cy="50.5" r="12.5"/><circle class="die__dot" cx="74.5" cy="75.5" r="12.5"/></g>`,
+  // Die 6
+  `<g id="dice-6"><rect class="die" width="150" height="150" rx="20"/><circle class="die__dot" cx="100.5" cy="74.5" r="12.5"/><circle class="die__dot" cx="49.5" cy="74.5" r="12.5"/><circle class="die__dot" cx="49.5" cy="43.5" r="12.5"/><circle class="die__dot" cx="100.5" cy="105.5" r="12.5"/><circle class="die__dot" cx="49.5" cy="105.5" r="12.5"/><circle class="die__dot" cx="99.5" cy="43.5" r="12.5"/></g>`,
+];
--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Selectors for the two score elements
+const score1El = document.querySelector('#score--0'); // Dom element for score 1
+const score2El = document.getElementById('score--1'); // Dom element for score 2
+
+score1El.textContent = 1;
+score2El.textContent = 1;
+
+const svg = document.querySelector('.svg');
+
+document.querySelector('.svg').innerHTML = dice[5];
